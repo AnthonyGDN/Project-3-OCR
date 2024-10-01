4dc3fdaa63953b2e318ade790c5a8da28921a16e
Anthony GDN <anthony.gdn0@gmail.com>
2024-10-01 16:17:26 +0200

    refactor + test - Rearrangement of css for better reading. Non-functional test of script to retrieve and display the gallery via js

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,11 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("test");
-    async function getWorks() {
-        const response = await fetch("http://localhost:5678/api/works");
-        const works = await response.json();
-        console.log(works);
-    }
-    getWorks();
-    const gallery = document.getElementById("gallery");
-    console.log(gallery);
-});
+const gallery = document.querySelector("gallery");
+async function getWorks() {
+    const response = await fetch("http://localhost:5678/api/works");
+    return await response.json();
+}
+async function displayWorks() {
+    gallery.innerHTML = ""
+    const arrayWorks = await getWorks();
+    arrayWorks.forEach((work) => {
+        const figure = document.createElement("figure");
+        const img = document.createElement("img");
+        const figcaption = document.createElement("figcaption");
+        img.src = work.imageUrl;
+        figcaption.textContent = work.title;
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+        gallery.appendChild(figure);
+    });
+}
+displayWorks();

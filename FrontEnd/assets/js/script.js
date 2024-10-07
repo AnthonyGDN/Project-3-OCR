49fb3a8ea5f34850f62edefe26a9ff3fe009417a
Anthony GDN <anthony.gdn0@gmail.com>
2024-10-07 20:32:52 +0200

    FIX - I fixed the script so that the works are displayed on the web page. FEAT - I added an Tous button to the html file, modified the CSS so that all the buttons appear as requested.

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -1,12 +1,30 @@
-const gallery = document.querySelector("gallery");
-async function getWorks() {
-    const response = await fetch("http://localhost:5678/api/works");
-    return await response.json();
-}
-async function displayWorks() {
-    gallery.innerHTML = ""
-    const arrayWorks = await getWorks();
-    arrayWorks.forEach((work) => {
+document.addEventListener("DOMContentLoaded", () => {
+    const gallery = document.querySelector(".gallery");
+    const buttonsFilter = document.querySelector(".buttonsFilter");
+
+    async function getWorks() {
+        try {
+            const response = await fetch("http://localhost:5678/api/works");
+            if (!response.ok) {
+                throw new Error('Error while retrieving works');
+            }
+            return await response.json();
+        } catch (error) {
+            console.error(error.message);
+            return [];
+        }
+    }
+    
+    async function displayWorks() {
+        const works = await getWorks();
+        gallery.innerHTML = "";
+        works.forEach((work) => {
+            createWorks(work);
+        });
+    }
+    
+    displayWorks();
+    function createWorks(work) {
         const figure = document.createElement("figure");
         const img = document.createElement("img");
         const figcaption = document.createElement("figcaption");
@@ -15,6 +33,35 @@ async function displayWorks() {
         figure.appendChild(img);
         figure.appendChild(figcaption);
         gallery.appendChild(figure);
-    });
-}
-displayWorks();
+    }
+
+    async function getCategories() {
+        try {
+            const response = await fetch("http://localhost:5678/api/categories");
+            if (!response.ok) {
+                throw new Error('Error while retrieving categories');
+            }
+            return await response.json();
+        } catch (error) {
+            console.error(error.message);
+            return [];
+        }
+    }
+    
+    async function displayButtonsFilter() {
+        const categories = await getCategories();
+        categories.forEach((category) => {
+            createButtonFilter(category);
+        });
+    }
+    
+    function createButtonFilter(category) {
+        const button = document.createElement("button");
+        button.innerText = category.name
+        buttonsFilter.appendChild(button);
+    }
+    displayButtonsFilter();
+});
+
+
+
